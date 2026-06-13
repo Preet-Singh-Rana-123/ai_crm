b65a71e3eb4ce200a8c28461f14fa4fcb422b3ce
Preet Rana <preetrana1263@gmail.com>
2026-06-13 16:54:19 +0530

    made function to get order

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -40,3 +40,50 @@ const createOrder = async (req, res) => {
         });
     }
 };
+
+const getOrders = async (req, res) => {
+    try {
+        const orders = await Order.find()
+            .populate("customerId", "name email city")
+            .sort({
+                createdAt: -1,
+            });
+
+        res.status(200).json({
+            success: true,
+            count: orders.length,
+            data: orders,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
+
+const getOrderById = async (req, res) => {
+    try {
+        const order = await Order.findById(req.params.id).populate(
+            "customerId",
+            "name email city",
+        );
+
+        if (!order) {
+            return res.status(404).json({
+                success: false,
+                message: "Order not found",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: order,
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+};
