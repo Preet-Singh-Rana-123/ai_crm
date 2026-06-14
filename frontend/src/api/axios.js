b7883a4c34d3d3d5693c412181dce882cc0f661e
Preet Rana <preetrana1263@gmail.com>
2026-06-14 21:19:56 +0530

    updated fronted base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://smartreach-api.onrender.com/api",
+    baseURL: "https://ai-crm-backend-72h2.onrender.com/api",
 
     headers: {
         "Content-Type": "application/json",
